2df1c6e5aa406f178ab5eaa4847ef810ddff61c8
levi isack eliezer <leviisac@g.jct.ac.il>
2019-03-17 20:38:39 +0200

    teste

---
+++ src/targuil/pkg1/primitives/Spacepoint.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package targuil.pkg1.primitives;
+
+/**
+ *
+ * @author levi''s pc
+ */
+public class Spacepoint {
+    
+}
